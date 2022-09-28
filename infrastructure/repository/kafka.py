8c5b8bb18fb5f0bf9be0f870b61d7450ff400b09
AdoniasAlcantara <adoniasasneto@gmail.com>
2022-09-28 17:20:17 -0300

    fix: instantiate a Kafka producer for each process

--- infrastructure/repository/kafka.py
+++ infrastructure/repository/kafka.py
@@ -5,9 +5,14 @@ from domain.repository import IPublisher
 
 class KafkaProducer(IPublisher):
     def __init__(self, config: dict):
-        self._producer = Producer(config)
+        self._config = config
+        self._producer = None
 
     def send(self, topic: str, data: any, key: str = None) -> str:
+        # Workaround to deal with Kafka producer multiprocessing issues
+        if self._producer is None:
+            self._producer = Producer(self._config)
+
         self._producer.produce(topic, key=key, value=data)
         self._producer.flush()
 
